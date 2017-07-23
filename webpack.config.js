5fc1c1ee0fe042d66ad2854124b4e7e3e430835d
Haskicbr <haskicbr@gmail.com>
2017-07-24 00:59:31 +0500

    add stable env webpack for babel;

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src'), // ТОЧКА ВХОДА
     output: {
         filename: 'bundle.js', // ФАйл выхода
-        path: path.resolve(__dirname, 'dist') 
+        path: path.resolve(__dirname, 'dist')
     },
 
     //Babel config
@@ -17,7 +17,7 @@ module.exports = {
                     options : {
                         // Установка других плагинов
                         // http://babeljs.io/docs/plugins
-                        presets : ['stage-0', 'es2016', 'es2015', "babili"]
+                        presets : ['env', 'stage-0', 'es2016', 'es2015', "babili"]
                     }
                 }
             }
